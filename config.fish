8001eb930f57d841dfa492817130e257aa8c0b88
Filipo <filgreen@pm.me>
2024-11-20 18:34:25 +0100

    Fix ohmyposh

--- config.fish
+++ config.fish
@@ -6,4 +6,4 @@ end
 export PATH="$PATH:/opt/nvim-linux64/bin"
 
 #starship init fish | source
-oh-my-posh init fish --config "ohmyposh/themes/emodipt-extend.omp.json" | source
+oh-my-posh init fish --config "~/.config/fish/ohmyposh/themes/emodipt-extend.omp.json" | source
